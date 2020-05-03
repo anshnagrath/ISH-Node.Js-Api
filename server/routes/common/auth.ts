8fbcf0e2a7eef2a2600f5371df1ff9bafb39e049
Ansh nagrath <34484528+anshnagrath@users.noreply.github.com>
2020-05-03 14:13:45 +0530

    Delete auth.ts

--- server/routes/common/auth.ts
+++
@@ -1,7 +0,0 @@
-export const config = {
-    "facebookAuth": {
-        "clientId": "1375514229250365",
-        "clientSecret": "9275b69b504310fadfd78917d20d7142",
-        "callbackUrl": "http://localhost:4000/api/auth/facebook/callback"
-    }
-}
